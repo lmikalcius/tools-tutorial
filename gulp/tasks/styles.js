fbc31b0696bfe58aa2c8f3868c8743f9bf3ef066
linas1985 <linas.mikalcius@gmail.com>
2018-03-07 20:09:03 -0600

    Add mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@ var autoprefixer = require("autoprefixer");
 var simpleVars = require("postcss-simple-vars");
 var nested = require("postcss-nested");
 var cssImport = require("postcss-import");
+var mixins = require("postcss-mixins");
 
 gulp.task("styles", function() {
   console.log("Something useful for CSS here");
   return gulp.src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport, simpleVars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, simpleVars, nested, autoprefixer]))
     .on("error", function (errorInfo) {
       console.log(errorInfo.toString());
       this.emit("end");
